2f84e1636dbba94a409cd3e152ebe61ad98ed5de
Gabriel Lee <gabriel.lee@u.nus.edu>
2023-09-06 08:57:35 +0800

    Update layout based on admin portal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,35 @@
-export default function Navbar() {
+import React, { useState } from "react";
+import { Menu, Button } from "antd";
+import Sider from "antd/es/layout/Sider";
+
+function Navbar(props) {
+    const [collapsed, setCollapsed] = useState(false);
     return (
-        <nav></nav>
+        <Sider
+            theme="light"
+            breakpoint="lg"
+            collapsedWidth="0"
+            onBreakpoint={(broken) => {
+                console.log(broken);
+            }}
+            onCollapse={(collapsed, type) => {
+                console.log(collapsed, type);
+                setCollapsed(collapsed)
+            }}
+            collapsible collapsed={collapsed}
+        >
+            {/*<div className="demo-logo-vertical" />*/}
+            <Menu
+                theme="light"
+                defaultSelectedKeys={['1']}
+                mode="inline"
+                selectedKeys={[props.currentTab]}
+                items={props.menuItems}
+                onClick={props.onClickNewTab}
+                style={{display: 'flex', flexDirection: 'column', width: '100%'}}
+            />
+        </Sider>
     )
 }
+
+export default Navbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,19 @@
+import {Layout} from 'antd';
+import React from "react";
+import CustomHeader from "../components/CustomHeader";
+import {Content} from "antd/es/layout/layout";
+
 export default function Home() {
+
+
     return (
-        <h1>Home</h1>
+            <Layout style={{minHeight: '100vh'}}>
+                <CustomHeader text={"Header"} />
+
+                <Content style={{ margin: '24px 16px 0' }}>
+                    <div style={{ padding: 24, minHeight: 360 }}>content</div>
+                </Content>
+
+            </Layout>
     )
 }
