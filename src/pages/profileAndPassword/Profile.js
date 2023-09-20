41a8d1a58feaca0ca905686cf367be460c4375cf
Albunist <61377132+Albunist@users.noreply.github.com>
2023-09-20 21:23:27 +0800

    Implemented top up and wallet

--- src/pages/profileAndPassword/Profile.js
+++ src/pages/profileAndPassword/Profile.js
@@ -21,6 +21,7 @@ import AddBankAccountModal from "./AddBankAccountsModal";
 import { loadStripe  } from '@stripe/stripe-js';
 import { useStripe } from '@stripe/react-stripe-js';
 import { vendorStaffApi,paymentApi } from "../../redux/api";
+import WalletModal from "./WalletModal";
 
 window.Buffer = window.Buffer || require("buffer").Buffer;
 
@@ -61,6 +62,10 @@ export default function Profile() {
     const [isChangePasswordModalOpen, setIsChangePasswordModalOpen] = useState(false); // change password boolean
     const [isBAModalOpen, setIsBAModalOpen] = useState(false);
     const [bankAccounts, setBankAccounts] = useState([]); // bank accounts
+    const [isTopUpModalOpen, setIsTopUpModalOpen] = useState(false);
+    const [isWithdrawModalOpen, setIsWithdrawModalOpen] = useState(false);
+
+
     // when the edit profile button is clicked
     function onClickEditProfile() {
         setIsViewProfile(false);
@@ -148,10 +153,30 @@ export default function Profile() {
       setIsBAModalOpen(true);
     }
 
-  // close edit password modal
     function onClickCancelManageBAButton() {
-     setIsBAModalOpen(false);
+      setIsBAModalOpen(false);
+     }
+
+
+    function onClickCancelTopUpButton() {
+     setIsTopUpModalOpen(false);
     }
+
+    function onClickTopUpButton() {
+      setIsTopUpModalOpen(true);
+    }
+
+
+    function onClickCancelWithdrawButton() {
+     setIsWithdrawModalOpen(false);
+    }
+
+    function onClickWithdrawButton() {
+      setIsWithdrawModalOpen(true);
+    }
+
+ 
+    
 
     // when user edits password
     async function onClickSubmitNewPassword(val) {
@@ -214,6 +239,53 @@ export default function Profile() {
         });
     }
   }
+
+  async function onClickSubmitTopUp(topUpDetails) {
+
+    const userId = parseInt(user.user_id);
+    const amount = topUpDetails.amount;
+    const response = await (user.user_type === 'VENDOR_STAFF' ? vendorStaffApi : paymentApi).
+    post(`/topUpWallet/${userId}/${amount}`);
+    if (response.status) {
+
+      setIsTopUpModalOpen(false);
+      //window.location.reload(); //Temporary measure will directly update bankAccount state
+      toast.success('Amount topped up successfully!', {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 1500
+    });
+  
+  } else {
+      toast.error(response.data.errorMessage, {
+          position: toast.POSITION.TOP_RIGHT,
+          autoClose: 1500
+      });
+  }
+}
+
+async function onClickSubmitWithdraw(withdrawalDetails) {
+  const bank_account_id = withdrawalDetails.bankAccountId;
+  const amount = withdrawalDetails.amount;
+  const userId = parseInt(user.user_id);
+
+  const response = await (user.user_type === 'VENDOR_STAFF' ? vendorStaffApi : paymentApi).
+  post(`/withdrawWallet/${userId}/${bank_account_id}/${amount}`);
+  if (response.status) {
+
+    setIsTopUpModalOpen(false);
+    //window.location.reload(); //Temporary measure will directly update bankAccount state
+    toast.success('Amount topped up successfully!', {
+      position: toast.POSITION.TOP_RIGHT,
+      autoClose: 1500
+  });
+
+} else {
+    toast.error(response.data.errorMessage, {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 1500
+    });
+}
+}
 
   const deleteBankAccount = async (bank_account_id) => {
     try {
@@ -465,10 +537,10 @@ export default function Profile() {
                             </Row>
 
                             <Col span={8}>
-        <Button type="primary" onClick={() => { /* Logic for Top Up */ }}>Top Up</Button>
+        <Button type="primary" onClick={onClickTopUpButton}>Top Up</Button>
       </Col>
       <Col span={8}>
-        <Button type="primary" onClick={() => { /* Logic for Withdraw */ }}>Withdraw</Button>
+        <Button type="primary" onClick={onClickWithdrawButton}>Withdraw</Button>
       </Col>
                                               
                           </div>
@@ -822,6 +894,25 @@ export default function Profile() {
                     isBAModalOpen={isBAModalOpen}
                     onClickSubmitNewBankAccount={onClickSubmitNewBankAccount}
                     onClickCancelManageBAButton={onClickCancelManageBAButton}
+                />
+            }
+
+{isTopUpModalOpen && 
+                <WalletModal
+                    title="Top Up" 
+                    isModalOpen={isTopUpModalOpen}
+                    onClickSubmitButton={onClickSubmitTopUp}
+                    onClickCancelButton={onClickCancelTopUpButton}
+                />
+            }
+
+{isWithdrawModalOpen && 
+                <WalletModal
+                    title="Withdraw"
+                    isModalOpen={isWithdrawModalOpen}
+                    onClickSubmitButton={onClickSubmitWithdraw}
+                    onClickCancelButton={onClickCancelWithdrawButton}
+                    bankAccounts={bankAccounts}
                 />
             }
 
--- src/pages/profileAndPassword/WalletModal.js
+++ src/pages/profileAndPassword/WalletModal.js
@@ -1,16 +1,21 @@
-import React from "react";
-import { Modal, Form, Input, Button } from "antd";
+import React, { useState } from "react";
+import { Modal, Form, Input, Button, InputNumber } from "antd";
 
 
 export default function WalletModal(props) {
+    const [selectedBankAccountId, setSelectedBankAccountId] = useState(null);
+
+    const handleRadioChange = (e) => {
+        setSelectedBankAccountId(e.target.value);
+      };
 
     return(
         <div>
             <Modal
-                title="Use Props"
+                title={props.title}
                 centered
-                open={props.isBAModalOpen}
-                onCancel={props.onClickCancelManageBAButton}
+                open={props.isModalOpen}
+                onCancel={props.onClickCancelButton}
                 footer={[]} // hide default buttons of modal
             >
                 <Form 
@@ -20,25 +25,48 @@ export default function WalletModal(props) {
                     style={{ maxWidth: 600 }}
                     required={true}
                     requiredMark={true}
-                    onFinish={props.onClickSubmitNewBankAccount}
+                    onFinish={props.onClickSubmitButton}
                 >
                     <Form.Item
-                    label="Bank Account Number"
+                    label="Amount"
                     labelAlign="left"
-                    name="bankAccountNumber"
-                    rules={[{ required: true, message: 'Please enter your bank account number!' }]}
+                    name="amount"
+                    rules={[{ required: true, message: 'Please enter the amount' }]}
                     >
-                    <Input placeholder="Bank Account Number"/>
+                    <InputNumber
+          min={0}
+          step={0.01}
+          precision={2}
+          formatter={(value) => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+          parser={(value) => value.replace(/\$\s?|(,*)/g, '')}
+
+        />
                     </Form.Item>
 
-                    <Form.Item
-                    label="Routing Number"
-                    labelAlign="left"
-                    name="routingNumber"
-                    rules={[{ required: true, message: 'Please enter the routing number!' },]}
-                    >
-                    <Input placeholder="Routing Number" />
-                    </Form.Item>
+                    {props.title === "Withdraw" && (
+
+                    <ul>
+                        
+  {props.bankAccounts && props.bankAccounts.length > 0 ? (
+    props.bankAccounts.map((account) => (
+      <li key={account.id} style={{ display: 'flex', alignItems: 'center' }}>
+        <input
+          type="radio"
+          name="selectedBankAccount"
+          value={account.id}
+          checked={selectedBankAccountId === account.id}
+          onChange={handleRadioChange}
+          style={{ marginRight: '10px' }}
+        />
+        Bank Account Number: *****{account.last4}
+       
+      </li>
+    ))
+  ) : (
+    <li>No bank accounts available.</li>
+  )}
+</ul>
+                    )}
 
                    
 
