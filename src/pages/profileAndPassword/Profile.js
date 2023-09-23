6e5d88a2380eb85166aefb6139afca667a349270
Albunist <61377132+Albunist@users.noreply.github.com>
2023-09-23 23:19:08 +0800

    Update bank account creation

--- src/pages/profileAndPassword/Profile.js
+++ src/pages/profileAndPassword/Profile.js
@@ -256,13 +256,14 @@ export default function Profile() {
       if (response.data) {
 
         setIsBAModalOpen(false);
+        setBankAccounts([...bankAccounts, response.data]);
          //Temporary measure will directly update bankAccount state
         toast.success('Bank account created successfully!', {
           position: toast.POSITION.TOP_RIGHT,
           autoClose: 1500
       });
 
-      window.location.reload();
+      
     
     } else {
       setIsBAModalOpen(false);
