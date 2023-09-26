d2cc9f99b3e9694dbe931cddaa63c5b7688f60af
Gabriel Lee <gabriel.lee@u.nus.edu>
2023-09-26 20:03:03 +0800

    Add in sorting and filtering function for view table

--- src/pages/tours/TourTypes.js
+++ src/pages/tours/TourTypes.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { React, useEffect, useState, useRef } from 'react';
 import { Layout, Form, Input, Badge, Space, Tag } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
@@ -10,6 +10,10 @@ import { ToastContainer, toast } from 'react-toastify';
 import CustomButton from "../../components/CustomButton";
 import { PlusOutlined } from "@ant-design/icons";
 import CreateTourTypeModal from "./CreateTourTypeModal";
+import ViewTourTypeModal from "./ViewTourTypeModal";
+import { Table, Button } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import Highlighter from 'react-highlight-words';
 
 export default function TourTypes() {
 
@@ -20,6 +24,8 @@ export default function TourTypes() {
     const [getTourTypesData, setGetTourTypesData] = useState(true);
     const [tourTypes, setTourTypes] = useState([]);
     const [tourTypeImages, setTourTypeImages] = useState({});
+    const [selectedTourTypeId, setSelectedTourTypeId] = useState(null);
+
 
     const viewTourTypesBreadCrumb = [
         {
@@ -49,42 +55,161 @@ export default function TourTypes() {
         }
     }, [getTourTypesData]);
 
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef(null);
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+    const handleReset = (clearFilters) => {
+        clearFilters();
+        setSearchText('');
+    };
+    const getColumnSearchProps = (dataIndex) => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+            <div
+                style={{
+                    padding: 8,
+                }}
+                onKeyDown={(e) => e.stopPropagation()}
+            >
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                    style={{
+                        marginBottom: 8,
+                        display: 'block',
+                    }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => clearFilters && handleReset(clearFilters)}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            confirm({
+                                closeDropdown: false,
+                            });
+                            setSearchText(selectedKeys[0]);
+                            setSearchedColumn(dataIndex);
+                        }}
+                    >
+                        Filter
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            close();
+                        }}
+                    >
+                        Close
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered) => (
+            <SearchOutlined
+                style={{
+                    color: filtered ? '#1677ff' : undefined,
+                }}
+            />
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownOpenChange: (visible) => {
+            if (visible) {
+                setTimeout(() => searchInput.current?.select(), 100);
+            }
+        },
+        render: (text) =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{
+                        backgroundColor: '#ffc069',
+                        padding: 0,
+                    }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ''}
+                />
+            ) : (
+                text
+            ),
+    });
+
     const tourTypesColumns = [
         {
             title: 'Name',
             dataIndex: 'name',
             key: 'name',
-            width: 70
+            width: 70,
+            sorter: (a, b) => a.name.localeCompare(b.name),
+            ...getColumnSearchProps('name'),
         },
         {
             title: 'Description',
             dataIndex: 'description',
             key: 'description',
-            width: 100
+            width: 100,
+            sorter: (a, b) => a.description.localeCompare(b.description),
+            ...getColumnSearchProps('description'),
         },
         {
             title: 'Price',
             dataIndex: 'price',
             key: 'price',
-            width: 40
+            width: 40,
+            sorter: (a, b) => a.description.localeCompare(b.description),
+            ...getColumnSearchProps('price'),
         },
         {
             title: 'No. of Pax',
             dataIndex: 'recommended_pax',
             key: 'recommended_pax',
-            width: 40
+            width: 50,
+            sorter: (a, b) => a.recommended_pax - b.recommended_pax,
+            ...getColumnSearchProps('recommended_pax'),
         },
         {
             title: 'Est. Duration',
             dataIndex: 'estimated_duration',
             key: 'estimated_duration',
-            width: 40
+            width: 50,
+            sorter: (a, b) => a.estimated_duration - b.estimated_duration,
+            ...getColumnSearchProps('estimated_duration'),
         },
         {
             title: 'Special Notes',
             dataIndex: 'special_note',
             key: 'special_note',
-            width: 100
+            width: 90,
+            sorter: (a, b) => a.special_note - b.special_note,
+            ...getColumnSearchProps('special_note'),
         },
         {
             title: 'Published',
@@ -97,7 +222,22 @@ export default function TourTypes() {
                     return <Badge status="error" text="No" />
                 }
             },
-            width: 100,
+            width: 40
+        },
+        {
+            title: 'Action(s)',
+            dataIndex: 'operation',
+            key: 'operation',
+            render: (text, record) => {
+                return <Space>
+                    <CustomButton
+                        text="View"
+                        onClick={() => onClickOpenViewTourTypeModal(record.tour_type_id)}
+                    />
+
+                </Space>
+            },
+            width: 70,
         }
     ];
 
@@ -141,6 +281,17 @@ export default function TourTypes() {
                 autoClose: 1500
             });
         }
+    }
+
+    const [isViewTourTypeModalOpen, setIsViewTourTypeModalOpen] = useState(false); 
+
+    function onClickOpenViewTourTypeModal(tourTypeId) {
+        setSelectedTourTypeId(tourTypeId);
+        setIsViewTourTypeModalOpen(true);
+    }
+
+    function onClickCancelViewTourTypeModal() {
+        setIsViewTourTypeModalOpen(false);
     }
 
     const updateTourTypeImages = (tourTypeId, imageFileName) => {
@@ -189,6 +340,13 @@ export default function TourTypes() {
                             onClickSubmitTourTypeCreate={onClickSubmitTourTypeCreate}
                             onUpdateImages={updateTourTypeImages}
                             onRemoveImage={removeTourTypeImage}
+                        />
+
+                        {/* Modal to view tour type */}
+                        <ViewTourTypeModal
+                            isViewTourTypeModalOpen={isViewTourTypeModalOpen}
+                            onClickCancelViewTourTypeModal={onClickCancelViewTourTypeModal}
+                            tourTypeId={selectedTourTypeId}
                         />
                     </Content>
                 </Layout>
