1cb1fee040e6f071586588ef299d7d57602c59dc
rosejtec <rosej.tec@gmail.com>
2023-09-27 20:06:45 +0800

    View Deal working

--- src/pages/deals/ViewDealModal.js
+++ src/pages/deals/ViewDealModal.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { Modal, Select, Tag, Badge, Carousel, Spin } from "antd";
 import { getDealById } from "../../redux/dealRedux";
+import moment from "moment";
 
 export default function ViewDealModal(props) {
 
-    const [deal, setDeal] = useState();
+    const [selectedDeal, setSelectedDeal] = useState([]);
 
     useEffect(() => {
         if (props.selectedDealId) {
             const fetchData = async (id) => {
                 const response = await getDealById(id);
                 if (response.status) {
-                    setDeal(response.data);
-                    
+                    setSelectedDeal(response.data);
+
                 } else {
                     console.log("Deal not fetched!");
                 }
@@ -22,79 +23,58 @@ export default function ViewDealModal(props) {
         }
     }, [props.selectedDealId]);
 
-    function getTypeColor(type) {
-        switch (type) {
-            case 'PHYSICALSIM':
-                return 'magenta';
-            case 'ESIM':
-                return 'cyan';
-            default:
-                return 'default';
+    function renderDealImage(imageList) {
+        if (Array.isArray(imageList) && imageList.length > 0) {
+            // Render a Carousel of images if there are multiple images
+            if (imageList.length > 1) {
+                return (
+                    <div style={styles.carousel}>
+                        <Carousel autoplay arrows>
+                            {imageList.map((imageUrl, index) => (
+                                <div key={index}>
+                                    <div style={styles.container}>
+                                        <img
+                                            src={imageUrl}
+                                            alt={`Attraction ${index + 1}`}
+                                            style={styles.image}
+                                        />
+                                    </div>
+                                </div>
+                            ))}
+                        </Carousel>
+                    </div>
+                );
+            } else {
+                // Display a single image if there is only one in the list
+                return (
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '35px' }}>
+                        <img
+                            src={imageList[0]}
+                            alt="Attraction"
+                            style={{ maxWidth: '400px', maxHeight: '300px', width: '100%', height: 'auto' }}
+                        />
+                    </div>
+                );
+            }
         }
-    }
-
-    function getPriceTierCount(count) {
-        switch (count) {
-            case 'TIER_1':
-                return '$';
-            case 'TIER_2':
-                return '$$';
-            case 'TIER_3':
-                return '$$$';
-            case 'TIER_4':
-                return '$$$$';
-            case 'TIER_5':
-                return '$$$$$';
-            default:
-                return 'Bug';
-        }
-    }
-
-    function getValidityColor(type) {
-        switch (type) {
-            case 'ONE_DAY':
-                return 'green';
-            case 'THREE_DAY':
-                return 'cyan';
-            case 'SEVEN_DAY':
-                return 'blue';
-            case 'FOURTEEN_DAY':
-                return 'geekblue';
-            case 'MORE_THAN_FOURTEEN_DAYS':
-                return 'purple';
-            default:
-                return 'default';
-        }
-    }
-
-    function getDataLimitColor(type) {
-        switch (type) {
-            case 'VALUE_10':
-                return 'magenta';
-            case 'VALUE_30':
-                return 'red';
-            case 'VALUE_50':
-                return 'volcano';
-            case 'VALUE_100':
-                return 'orange';
-            case 'UNLIMITED':
-                return 'gold';
-            default:
-                return 'default';
-        }
+        return 'No Image';
     }
 
     function renderProperty(label, value, color) {
 
-        let formattedValue = value;
-
         return (
             <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', marginBottom: '16px' }}>
-                <div style={{ fontWeight: 'bold', minWidth: '160px' }}>{label}:</div>
+                <div style={{ fontWeight: 'bold', minWidth: '200px' }}>{label}:</div>
                 <div>
-                    {label === 'Published' && value === true && <Badge status="success" text="Yes" />}
-                    {label === 'Published' && value === false && <Badge status="error" text="No" />}
-                    {color ? <Tag color={color}>{formattedValue}</Tag> : formattedValue}
+                    {label === 'Is Published?' || label === 'Is Government Voucher?' ? (
+                        <Badge status={value ? 'success' : 'error'} text={value ? 'Yes' : 'No'} />
+                    ) : (
+                        color ? (
+                            <Tag color={color}>{value}</Tag>
+                        ) : (
+                            value
+                        )
+                    )}
                 </div>
             </div>
         );
@@ -102,25 +82,26 @@ export default function ViewDealModal(props) {
 
     return (
         <div>
-            {deal &&
             <Modal
-                title={deal.name}
+                title="View Deal"
                 centered
                 open={props.viewDealModal}
                 onCancel={props.onCancelViewModal}
                 footer={[]} // hide default buttons of modal
             >
                 <div style={{ display: 'flex', flexDirection: 'column', padding: '20px' }}>
-                    {renderProperty('Description', deal.description)}
-                    {renderProperty('Published', deal.is_published )}
-                    {renderProperty('Price', deal.price)}
-                    {renderProperty('Price Tier', getPriceTierCount(deal.estimated_price_tier))}
-                    {renderProperty('Duration', deal.num_of_days_valid)}
-                    {renderProperty('Duration Category', deal.plan_duration_category, getValidityColor(deal.plan_duration_category))}
-                    {renderProperty('Data Limit', deal.data_limit)}
-                    {renderProperty('Data Limit Category', deal.data_limit_category, getDataLimitColor(deal.data_limit_category))}
+                    {renderDealImage(selectedDeal.deal_image_list)}
+                    {renderProperty('Promo Code', selectedDeal.promo_code)}
+                    {renderProperty('Deal Type', selectedDeal.deal_type, 'blue')}
+                    {renderProperty('Discount Percentage', selectedDeal.discount_percent + '%', 'green')}
+                    {renderProperty('Start DateTime', moment(selectedDeal.start_datetime).format('llll'))}
+                    {renderProperty('End Day Time Group', moment(selectedDeal.end_datetime).format('llll'))}
+                    {renderProperty('Is Government Voucher?', selectedDeal.is_govt_voucher ? "Yes" : "No")}
+                    {renderProperty('Is Published?', selectedDeal.is_published ? "Yes" : "No")}
+                    {renderProperty('Publish Date', moment(selectedDeal.publish_date).format('ll'))}
+
                 </div>
-            </Modal>}
+            </Modal>
         </div>
     )
 }
