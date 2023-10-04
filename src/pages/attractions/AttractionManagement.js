1e15611e88f401803310547f30c8f4241d650db4
darryl <darrylgoh51@gmail.com>
2023-10-04 14:21:07 +0800

    fixed bugs

--- src/pages/attractions/AttractionManagement.js
+++ src/pages/attractions/AttractionManagement.js
@@ -96,19 +96,6 @@ export default function AttractionManagement() {
                         type="link"
                         size="small"
                         onClick={() => {
-                            confirm({
-                                closeDropdown: false,
-                            });
-                            setSearchText(selectedKeys[0]);
-                            setSearchedColumn(dataIndex);
-                        }}
-                    >
-                        Filter
-                    </Button>
-                    <Button
-                        type="link"
-                        size="small"
-                        onClick={() => {
                             close();
                         }}
                     >
