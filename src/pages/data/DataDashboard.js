a06b5e07c9fa4f4d475c8078c583dd885ed0cfe9
Leah James <60879267+rosejtec@users.noreply.github.com>
2023-11-04 20:34:20 +0800

    Leah/charts (#57)
    * Responsive + created time series file
    
    * Add change by xaxisdropdwon
    
    * Add data use case dropdown

--- src/pages/data/DataDashboard.js
+++ src/pages/data/DataDashboard.js
@@ -1,305 +1,273 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { DownOutlined, SmileOutlined, DashboardOutlined } from '@ant-design/icons';
-import { Dropdown, Button, Menu, Layout } from 'antd';
+import React, {useState, useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {DownOutlined, SmileOutlined, DashboardOutlined} from '@ant-design/icons';
+import {Dropdown, Button, Menu, Layout, Typography, Select} from 'antd';
 import 'chartjs-adapter-date-fns'; // Import the date adapter
 import SubscriptionModal from "./SubscriptionModal";
-import CustomButton from "../../components/CustomButton";import CustomHeader from "../../components/CustomHeader";
-import { Content } from "antd/es/layout/layout";
+import CustomButton from "../../components/CustomButton";
+import CustomHeader from "../../components/CustomHeader";
+import {Content} from "antd/es/layout/layout";
 
-import { Chart as ChartJS, LineController,LineElement, PointElement, CategoryScale, LinearScale, Title, Tooltip, Legend,TimeScale} from 'chart.js';
-import { Bar, Line } from 'react-chartjs-2';
-import { getData, subscribe, getSubscription, getSubscriptionStatus } from "../../redux/dataRedux";
-import { set } from 'date-fns';
-import { ToastContainer, toast } from 'react-toastify';
+import {
+    Chart as ChartJS,
+    LineController,
+    LineElement,
+    PointElement,
+    CategoryScale,
+    LinearScale,
+    Title,
+    Tooltip,
+    Legend,
+    TimeScale
+} from 'chart.js';
+import {Bar, Line} from 'react-chartjs-2';
+import {getData, subscribe, getSubscription, getSubscriptionStatus} from "../../redux/dataRedux";
+import {set} from 'date-fns';
+import {ToastContainer, toast} from 'react-toastify';
+import {TotalBookingsTimeSeries} from "./TotalBookingsTimeSeries.js";
 
 ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  TimeScale,
-  LineController,
-  Title,
-  Tooltip,
-  Legend
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    TimeScale,
+    LineController,
+    Title,
+    Tooltip,
+    Legend
 );
 
+const TOTAL_BOOKINGS_OVER_TIME = "Total Bookings Over Time";
+const REVENUE_OVER_TIME = "Revenue Over Time";
+const BOOKINGS_BREAKDOWN = "Bookings Breakdown by Activity, Nationality, Age";
+const REVENUE_BREAKDOWN = "Revenue Breakdown by Activity, Nationality, Age";
+const CUSTOMER_RETENTION = "Customer Retention (Number of Repeat Bookings Over Time)";
+
 const DataDashboard = () => {
-  const [isSubscribed, setIsSubscribed] = useState(true);
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
-  const [data, setData] = useState([]);
+    const [isSubscribed, setIsSubscribed] = useState(true);
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
+    const [data, setData] = useState([]);
+    const [selectedDataUseCase, setSelectedDataUseCase] = useState(TOTAL_BOOKINGS_OVER_TIME);
 
-  const dataBreadCrumb = [
-    {
-      title: 'Dashboard',
-    }
-];
-  //const [subscriptionDetails, setSubscriptionDetails] = useState(null);
-  const [operation, setOperation] = useState("SUBSCRIBE");
+    const dataBreadCrumb = [
+        {
+            title: 'Dashboard',
+        }
+    ];
+    //const [subscriptionDetails, setSubscriptionDetails] = useState(null);
+    const [operation, setOperation] = useState("SUBSCRIBE");
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const onClickViewSubButton = () => {
-    navigate('/datadashboard/subscription'); // Replace with your route
-  };
-
-  // Subscription 
-  const [isSubModalOpen, setIsSubModalOpen] = useState(false);
-
-  function onClickCancelManageSubButton() {
-    setIsSubModalOpen(false);
-  }
-
-  function onClickManageSubButton() {
-    setIsSubModalOpen(true);
-  }
-
-
-  // useEffect(() => {
-  //   // Fetch user subscription status here
-  //   const fetchSubscriptionStatus = async () => {
-  //     try {
-  //       // Replace this with your API call to fetch user subscription status
-  //       const response = await getSubscriptionStatus(user.vendor.vendor_id, "VENDOR");
-
-  //       if (response.status) {
-  //         const subscribed = response.data;
-          
-  //         if (subscribed) {
-  //           setIsSubscribed(true);
-  //         }
-          
-  //       } else {
-  //         toast.error(response.data.errorMessage, {
-  //           position: toast.POSITION.TOP_RIGHT,
-  //           autoClose: 1500
-  //         });
-  //       }
-  //     } catch (error) {
-  //       toast.error(error, {
-  //         position: toast.POSITION.TOP_RIGHT,
-  //         autoClose: 1500
-  //       });
-  //     }
-  //   };
-
-  //   fetchSubscriptionStatus();
-  // }, []);
-
-  async function onClickSubmitSubscription(subscriptionDetails) {
-    try {
-
-      const response = await subscribe(user.vendor.vendor_id, "VENDOR", subscriptionDetails.subscriptionType, subscriptionDetails.autoRenew);
-      if (response.status) {
-        setIsSubscribed(true);
-      } else {
-        toast.error(response.data.errorMessage, {
-          position: toast.POSITION.TOP_RIGHT,
-          autoClose: 1500
-        });
-      }
-
-
-    } catch (error) {
-      toast.error(error, {
-        position: toast.POSITION.TOP_RIGHT,
-        autoClose: 1500
-      });
-    }
-
-
-  }
-
-  useEffect(() => {
-    // Fetch user subscription status here
-    const callGetData = async () => {
-      try {
-        // Replace this with your API call to fetch user subscription status
-        const response = await getData('3');
-        if (response.status) {
-          
-          setData(response.data)
-
-      } else {
-          console.log("Wat");
-          
-      }
-        
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
+    const onClickViewSubButton = () => {
+        navigate('/datadashboard/subscription'); // Replace with your route
     };
 
-    callGetData();
-  }, []);
+    // Subscription
+    const [isSubModalOpen, setIsSubModalOpen] = useState(false);
 
-  /* const data = ; */
+    function onClickCancelManageSubButton() {
+        setIsSubModalOpen(false);
+    }
 
-  const options = {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  };
+    function onClickManageSubButton() {
+        setIsSubModalOpen(true);
+    }
 
-  const items = [
-    {
-      key: '1',
-      label: (
-        <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
-          1st menu item
-        </a>
-      ),
-    },
-    {
-      key: '2',
-      label: (
-        <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
-          2nd menu item (disabled)
-        </a>
-      ),
-      icon: <SmileOutlined />,
-      disabled: true,
-    },
-    {
-      key: '3',
-      label: (
-        <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
-          3rd menu item (disabled)
-        </a>
-      ),
-      disabled: true,
-    },
-    {
-      key: '4',
-      danger: true,
-      label: 'a danger item',
-    },
-  ];
 
-  const aggregateDataByMonth = (data) => {
-    const aggregatedData = new Map(); // Use a Map to store aggregated data by month
-  
-    // Loop through the data and aggregate by month
-    data.forEach((item) => {
-      const [date, count] = item;
-      const monthKey = date.substr(0, 7); // Extract yyyy-MM part of the date
-  
-      if (aggregatedData.has(monthKey)) {
-        // Increment the count for the existing month
-        aggregatedData.set(monthKey, aggregatedData.get(monthKey) + count);
-      } else {
-        // Initialize the count for a new month
-        aggregatedData.set(monthKey, count);
-      }
-    });
-  
-    // Convert the aggregated Map back to an array of [month, count] pairs
-    const aggregatedArray = Array.from(aggregatedData, ([month, count]) => [month, count]);
-  
-    // Sort the array by month if needed
-    aggregatedArray.sort((a, b) => a[0].localeCompare(b[0]));
-  
-    return aggregatedArray;
-  };
-  
-  // Usage:
-  const aggregatedData = aggregateDataByMonth(data);
-  
+    // useEffect(() => {
+    //   // Fetch user subscription status here
+    //   const fetchSubscriptionStatus = async () => {
+    //     try {
+    //       // Replace this with your API call to fetch user subscription status
+    //       const response = await getSubscriptionStatus(user.vendor.vendor_id, "VENDOR");
 
-  const lineData = {
-    labels: aggregatedData.map(item => item[0]), // Convert dates to strings
-    datasets: [
-      {
-        label: 'Number of Bookings',
-        data: aggregatedData.map(item => item[1]),
-        borderColor: 'rgba(75, 192, 192, 1)',
-        borderWidth: 1,
-        fill: false,
-      },
-    ],
-  };
+    //       if (response.status) {
+    //         const subscribed = response.data;
 
-  console.log(data)
+    //         if (subscribed) {
+    //           setIsSubscribed(true);
+    //         }
 
-  console.log(data.map(item => item[1]))
+    //       } else {
+    //         toast.error(response.data.errorMessage, {
+    //           position: toast.POSITION.TOP_RIGHT,
+    //           autoClose: 1500
+    //         });
+    //       }
+    //     } catch (error) {
+    //       toast.error(error, {
+    //         position: toast.POSITION.TOP_RIGHT,
+    //         autoClose: 1500
+    //       });
+    //     }
+    //   };
 
-  const chartOptions = {
-    scales: {
-      x: {
-        type: 'time',
-        time: {
-          unit: 'month',
-          displayFormats: {
-            month: 'yyyy-MM',
-          },
+    //   fetchSubscriptionStatus();
+    // }, []);
+
+    async function onClickSubmitSubscription(subscriptionDetails) {
+        try {
+
+            const response = await subscribe(user.vendor.vendor_id, "VENDOR", subscriptionDetails.subscriptionType, subscriptionDetails.autoRenew);
+            if (response.status) {
+                setIsSubscribed(true);
+            } else {
+                toast.error(response.data.errorMessage, {
+                    position: toast.POSITION.TOP_RIGHT,
+                    autoClose: 1500
+                });
+            }
+
+
+        } catch (error) {
+            toast.error(error, {
+                position: toast.POSITION.TOP_RIGHT,
+                autoClose: 1500
+            });
+        }
+
+
+    }
+
+    useEffect(() => {
+        // Fetch user subscription status here
+        const callGetData = async () => {
+            try {
+                // Replace this with your API call to fetch user subscription status
+                const response = await getData('3');
+                if (response.status) {
+                    console.log(response.data)
+                    setData(response.data)
+
+                } else {
+                    console.log("Wat");
+
+                }
+
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        callGetData();
+    }, []);
+
+
+    const items = [
+        {
+            label: TOTAL_BOOKINGS_OVER_TIME,
+            value: TOTAL_BOOKINGS_OVER_TIME
         },
-      },
-      y: {
-        beginAtZero: true,
-      },
-    },
-  };
+        {
+            label: REVENUE_OVER_TIME,
+            value: REVENUE_OVER_TIME
 
-  
-  return (
-    <Layout style={styles.layout}>
-        <CustomHeader items={dataBreadCrumb} />
-        <Content style={styles.content}>
-          <div>
-            {isSubscribed ? (
-              <div>
-                <Dropdown menu={{
-                    items,
-                  }}>
-                <Button> Choose an Option </Button>
-                </Dropdown>
+        },
+        {
+            label: REVENUE_BREAKDOWN,
+            value: REVENUE_BREAKDOWN
 
-                <CustomButton text="Manage Subscription" icon={<DashboardOutlined />} onClick={onClickViewSubButton} />
-                <Line data={lineData} options={chartOptions}/>
-            </div>
-          ) : (
-        <div style={{ 
-                    display: 'flex', 
-                    flexDirection: 'column', 
-                    alignItems: 'center', 
-                    justifyContent: 'center' 
-                  }}>
+        },
+        {
+            label: BOOKINGS_BREAKDOWN,
+            value: BOOKINGS_BREAKDOWN
 
-            <p>Empower your business with data</p>
-            <CustomButton text="Subscribe Now" icon={<DashboardOutlined />} onClick={onClickManageSubButton} />
+        },
 
-            {isSubModalOpen &&
-            <SubscriptionModal
-              operation={operation}
-              isSubModalOpen={isSubModalOpen}
-              onClickSubmitSubscription={onClickSubmitSubscription}
-              onClickCancelManageSubButton={onClickCancelManageSubButton}
-            />
-          } 
-        </div>
-      )}    
-        </div>
-        </Content>
-      </Layout>
-  );
+    ];
+
+    const handleChangeDataUseCase = (value) => {
+        console.log(value); // { value: "lucy", label: "Lucy (101)" }
+        setSelectedDataUseCase(value.value)
+    };
+    // Usage:
+
+    const returnChart = () => {
+        if(selectedDataUseCase === TOTAL_BOOKINGS_OVER_TIME) {
+            return  <TotalBookingsTimeSeries data={data}/>
+        }
+    }
+
+    return (
+        <Layout style={styles.layout}>
+            <CustomHeader items={dataBreadCrumb}/>
+            <Content style={styles.content}>
+                <div>
+                    {isSubscribed ? (
+                        <div>
+                            <CustomButton style= {{margin: '10px'}}text="Manage Subscription" icon={<DashboardOutlined/>} onClick={onClickViewSubButton}/>
+
+                            <div style={styles.container}>
+                                <Typography.Title level={5} style={{marginRight: '10px'}}>Chart Type: </Typography.Title>
+                                <Select
+                                    labelInValue
+                                    defaultValue={items[0]}
+                                    style={{width: 400}}
+                                    onChange={handleChangeDataUseCase}
+                                    options={items}
+                                />
+                            </div>
+
+
+                            <br></br>
+                            <br></br>
+                            {returnChart()}
+
+                        </div>
+                    ) : (
+                        <div style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center'
+                        }}>
+
+                            <p>Empower your business with data</p>
+                            <CustomButton text="Subscribe Now" icon={<DashboardOutlined/>}
+                                          onClick={onClickManageSubButton}/>
+
+                            {isSubModalOpen &&
+                                <SubscriptionModal
+                                    operation={operation}
+                                    isSubModalOpen={isSubModalOpen}
+                                    onClickSubmitSubscription={onClickSubmitSubscription}
+                                    onClickCancelManageSubButton={onClickCancelManageSubButton}
+                                />
+                            }
+                        </div>
+                    )}
+                </div>
+            </Content>
+        </Layout>
+    );
 };
 
 export default DataDashboard;
 
 const styles = {
-  layout: {
-      minHeight: '100vh',
-      minWidth: '90vw',
-      backgroundColor: 'white'
-  },
-  content: {
-      margin: '1vh 3vh 1vh 3vh',
-      marginTop: -10, 
-      justifyContent: 'center', 
-      alignItems: 'center',
-      marginLeft: 57,
-  }
+    layout: {
+        minHeight: '100vh',
+        minWidth: '90vw',
+        backgroundColor: 'white'
+    },
+    content: {
+        margin: '1vh 3vh 1vh 3vh',
+        marginTop: -10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 57,
+    },
+    container: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    line: {
+        position: 'relative',
+        margin: 'auto',
+        maxWidth: '80vw',
+        height: '300px',
+        width: '100%'
+    }
 }
