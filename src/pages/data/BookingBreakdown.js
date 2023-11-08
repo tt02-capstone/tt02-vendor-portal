379de1a345d5c316a2ba0d7fa7fd500df981febe
rosejtec <rosej.tec@gmail.com>
2023-11-09 01:45:57 +0800

    in a line

--- src/pages/data/BookingBreakdown.js
+++ src/pages/data/BookingBreakdown.js
@@ -35,7 +35,12 @@ const AGE = 'Age';
 const STATUS = 'Status';
 const ACTIVITY = "Activity";
 export const BookingBreakdown = (props) => {
-    const data = props.data
+    const data = {
+        Status: {Completed: 89, Cancelled: 20, Upcoming: 35 , Ongoing: 50},
+        Category: {DeluxeSuite: 89, JuniorSuite: 20, Double: 20, Standard: 30},
+        Country: {China: 23, Malaysia: 13, Australia: 16, Indonesia: 19, India: 18}
+    }
+    // const data = props.data
     const [selectedItem, setSelectedItem] = useState(NATIONALITY);
     const chartRef = useRef(null);
 
@@ -83,27 +88,32 @@ export const BookingBreakdown = (props) => {
 
     const aggregatedData = aggregateDatafromDropdown(data);
 
-    const pieData = {
-        labels: Object.keys(aggregatedData),
-        datasets: [
-            {
-                data: Object.values(aggregatedData),
-                backgroundColor: [
-                    'red',
-                    'green',
-                    'blue',
-                    'orange',
-                    'purple',
-                    // Add more colors as needed
-                ],
-            },
-        ],
+    // const pieData = {
+    //     labels: Object.keys(aggregatedData),
+    //     datasets: [
+    //         {
+    //             data: Object.values(aggregatedData),
+    //             backgroundColor: [
+    //                 'red',
+    //                 'green',
+    //                 'blue',
+    //                 'orange',
+    //                 'purple',
+    //                 // Add more colors as needed
+    //             ],
+    //         },
+    //     ],
+    // };
+
+    const getRandomColors = (count) => {
+        // Generate random colors for the chart
+        const colors = [];
+        for (let i = 0; i < count; i++) {
+            colors.push(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
+        }
+        return colors;
     };
-
     console.log(data)
-
-    console.log(data.map(item => item[1]))
-
     const defaultChartOptions = {
         responsive: true,
         maintainAspectRatio: false,
@@ -114,26 +124,29 @@ export const BookingBreakdown = (props) => {
             title: {
                 display: true,
                 text: `Breakdown by ${selectedItem}`,
+                position: 'bottom'
             },
         },
     };
-    //
-    // const getChartOptions = () => {
-    //     const chartOptions = {
-    //         ...defaultChartOptions, // Start with the default options
-    //     };
-    //
-    //     // Adjust time unit-specific settings
-    //     if (selectedXAxis === WEEKLY) {
-    //         chartOptions.scales.x.time.unit = 'week';
-    //     } else if (selectedXAxis === YEARLY) {
-    //         chartOptions.scales.x.time.unit = 'year';
-    //     } else if (selectedXAxis === MONTHLY) {
-    //         chartOptions.scales.x.time.unit = 'month';
-    //     }
-    //
-    //     return chartOptions;
-    // };
+
+    const getChartOptions = (key) => {
+        const chartOptions = {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: `Breakdown by ${key}`,
+                    position: 'bottom'
+                },
+            },
+        };
+
+        return chartOptions;
+    };
 
     const handleChangeDropdown = (value) => {
         console.log(value); // { value: "lucy", label: "Lucy (101)" }
@@ -145,57 +158,88 @@ export const BookingBreakdown = (props) => {
         // }
     };
 
+    const renderCharts = () => {
+        Object.keys(data).map(key => {
+            console.log(key)
+        });
+    };
+
 
     return (
         <>
 
             <div style={styles.container}>
-                <Typography.Title level={5} style={{marginRight: '10px'}}>Breakdown by: </Typography.Title>
-                <Select
-                    labelInValue
-                    defaultValue={itemsDropdown[0]}
-                    style={{width: 120}}
-                    onChange={handleChangeDropdown}
-                    options={itemsDropdown}
-                />
+                {/*<Typography.Title level={5} style={{marginRight: '10px'}}>Breakdown by: </Typography.Title>*/}
+                {/*<Select*/}
+                {/*    labelInValue*/}
+                {/*    defaultValue={itemsDropdown[0]}*/}
+                {/*    style={{width: 120}}*/}
+                {/*    onChange={handleChangeDropdown}*/}
+                {/*    options={itemsDropdown}*/}
+                {/*/>*/}
 
             </div>
             <br></br>
 
-            <div style={styles.line}>
-                <Pie
-                    ref={chartRef}
-                    data={pieData}
-                    options={defaultChartOptions}
-                />
+            <div style={styles.content}>
+                {Object.keys(data).map((key) => {
+                    const labels = Object.keys(data[key]);
+                    const values = Object.values(data[key]);
+
+                    const pieData = {
+                        labels,
+                        datasets: [
+                            {
+                                data: values,
+                                backgroundColor: getRandomColors(values.length),
+                            },
+                        ],
+                    };
+
+                    return (
+                        <div key={key} style={styles.line}>
+                            {/*<h3>{key}</h3>*/}
+                            <Pie data={pieData} options={getChartOptions(key)} />
+                        </div>
+                    );
+                })}
             </div>
-        </>
+            </>
     );
 };
-
 
 const styles = {
     layout: {
         minHeight: '100vh',
         minWidth: '90vw',
-        backgroundColor: 'white'
+        backgroundColor: 'white',
     },
     container: {
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     content: {
+        flex: 'calc(50% - 20px)', // Set each chart to take 50% width with spacing
         margin: '1vh 3vh 1vh 3vh',
         marginTop: -10,
         justifyContent: 'center',
         alignItems: 'center',
-        marginLeft: 57,
+        marginLeft: 10,
     },
     line: {
-        position: 'relative',
-        margin: 'auto',
-        maxWidth: '80vw',
-        height: '300px',
-        width: '100%'
+        display: 'flex',
+        flexDirection: 'row',
+        margin: '0 20px', // Increase the margin to add spacing between charts
+    },
+    chart: {
+        flex: 1,
+        margin: '0 20px', // Increase the margin to add spacing between charts
+
+    },
+    gridContainer: {
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr', // Creates a 2x2 grid
+        gap: '20px', // Adjust the gap as needed for spacing
     }
-}
+};
