40f6675ac4a15964f91fd5de258c73b661731310
Ci En <ciensim@gmail.com>
2023-11-13 23:48:15 +0800

    ui fixes

--- src/pages/forum/forumCategory.js
+++ src/pages/forum/forumCategory.js
@@ -33,7 +33,7 @@ export default function ForumCategory() {
         <Layout style={styles.layout}>
              <CustomHeader items={forumBreadCrumb} />
              <Content style={styles.content}>
-                <div style={{ fontWeight: "bold", fontSize: 26, marginBottom: 10}}> Connect with Our Fellow Users </div> <br/>
+                <div style={{ fontWeight: "bold", fontSize: 26, marginBottom: 10, marginTop:10}}> Connect with Our Fellow Users </div> <br/>
 
                 <List
                     itemLayout="horizontal"
