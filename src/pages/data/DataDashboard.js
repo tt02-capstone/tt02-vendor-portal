e98e2af262b38ed0a1bcc0e35e65567995c688c1
Albunist <e0518533@u.nus.edu>
2023-11-14 19:40:58 +0800

    Added toast message for sub

--- src/pages/data/DataDashboard.js
+++ src/pages/data/DataDashboard.js
@@ -641,6 +641,7 @@ const DataDashboard = () => {
                             <ToastContainer />
                         </div>
                     )}
+                    <ToastContainer />
                 </div>
             </Content>
         </Layout>
